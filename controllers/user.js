fdf3be7c92884d4cb0e8ef8b9468fb9cb12a4751
awez-018 <awezbinsuleman18@gmail.com>
2025-09-17 00:21:49 +0400

    refactored all the code, the code is now clean and readable and easy to navigate

---
+++ controllers/user.js
@@ -0,0 +1,57 @@
+const User = require("../models/user");
+
+async function handleGetAllUsers(req, res) {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
+}
+
+async function handleGetUserById(req, res) {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).json({ error: "user not found" });
+  return res.json(user);
+}
+
+async function handleUpdateUserById(req, res) {
+  //edit user with id
+  await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  return res.json({ status: "Success" });
+}
+
+async function handleDeleteUserById(req, res) {
+  //delete user with id
+  await User.findByIdAndDelete(req.params.id);
+  return res.json({ status: "success", message: `user ${id} deleted` });
+}
+
+async function handleCreateNewUser(req, res) {
+  //todo: create new user
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All fields are required..." });
+  }
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
+  });
+  console.log("result: ", result);
+  return res.status(201).json({ msg: "success", id: result._id });
+}
+
+module.exports = {
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
+};
--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const fs = require("fs");
+
+const app = express();
 
 const { connectMongoDb } = require("./connection");
 
+const { logReqRes } = require("./middlewares");
+
 const userRouter = require("./routes/user");
 
-const app = express();
 const PORT = 8000;
 
 //connection
 connectMongoDb("mongodb://127.0.0.1:27017/project-1");
-//middlewar - plugin
 
+//middlewar - plugin
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use((req, res, next) => {
-  console.log("TIme: ", Date.now());
-  next();
-});
+app.use(logReqRes("log.txt"));
 
 //Routes
 app.use("/user", userRouter);
